faa8a2ef86655cd051f752ab9880f62fa58affce
sandy100061 <sandy100060@gmail.com>
2024-12-30 19:39:57 -0600

    Configuration Testing

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,16 @@ namespace azure_app_sand.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        this.configuration = configuration;
     }
 
     public void OnGet()
     {
-
+        ViewData["Greetings"] = this.configuration.GetValue<string>("Greetings");
     }
 }
